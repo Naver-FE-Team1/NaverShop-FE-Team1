a477c316eda59b3dc0c66b42c3db88a269937219
Hoàng Vũ <81975442+VKH2801@users.noreply.github.com>
2022-10-06 15:00:18 +0700

    Push AboutPage
    Push AboutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import ShoppingList from './components/pages/ShoppingList'
+import AboutPage from './components/pages/AboutPage'
 
 function App() {
 
   return (
     <div className="App">
+      <AboutPage/>
       <ShoppingList />
     </div>
   )

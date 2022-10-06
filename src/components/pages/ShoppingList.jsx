9b4fea5000ae172f07807d500557fedb8c7688c5
PhamneyuT <koduapass@gmail.com>
2022-10-06 09:51:10 +0700

    hot fixed for missing container

--- src/components/pages/ShoppingList.jsx
+++ src/components/pages/ShoppingList.jsx
@@ -16,6 +16,8 @@ import Banner from '../organisms/Banner/Banner'
 import FiltersBox from '../organisms/Filters/FiltersBox'
 import ProductList from '../organisms/ProductList/ProductList'
 
+const container = window !== undefined ? () => window.document.body : undefined;
+
 const ShoppingList = () => {
     /*     To open filter drawer (only available for mobile size) */
     const [openDrawer, setOpenDrawer] = useState(false)

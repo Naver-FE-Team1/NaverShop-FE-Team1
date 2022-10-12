9dddc6184ee5b86802847632a886b21a24dd52e8
HaiDang1501 <haidangtr1501@gmail.com>
2022-10-13 00:24:53 +0700

    setup redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import App from './App'
 import './scss/index.scss'
 import { ThemeConfig } from './theme/ThemeConfig'
 import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Provider } from "react-redux";
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={ThemeConfig}>
       <CssBaseline />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 )

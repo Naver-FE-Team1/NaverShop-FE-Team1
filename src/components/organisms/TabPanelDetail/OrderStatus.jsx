e2665cf36a70cb3d958cecf3466f1388db16ad51
PhamneyuT <koduapass@gmail.com>
2022-10-18 10:14:45 +0700

    update order tracking table

--- src/components/organisms/TabPanelDetail/OrderStatus.jsx
+++ src/components/organisms/TabPanelDetail/OrderStatus.jsx
@@ -1,6 +1,11 @@
-import { Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, Typography } from '@mui/material'
+import { CloseOutlined } from '@mui/icons-material'
+import { Divider, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, Typography, useMediaQuery } from '@mui/material'
 import React, { useState } from 'react'
+import { ThemeConfig } from '../../../theme/ThemeConfig'
+import FootBasket from '../../molecules/FootBasket/FootBasket'
 import StatusChip from '../../molecules/StatusChip/StatusChip'
+import ShoppingBasket from '../../pages/ShoppingBasket/ShoppingBasket'
+import ProductCheckout from '../ProductCheckout/ProductCheckout'
 import "./Tabs.scss"
 
 const headersTable = [
@@ -37,11 +42,23 @@ const rowStyle = (fontSize = "16px", fontWeight = "400") => {
     }
 }
 
+const fontStyle = (fontSize = "16px", fontColor = "#000", fontWeight = "400") => {
+    return {
+        fontFamily: "Clash Display",
+        fontWeight: fontWeight,
+        fontSize: fontSize,
+        lineHeight: "140%",
+        color: fontColor,
+    }
+}
+
 const OrderStatus = () => {
     const [openModal, setOpenModal] = useState(false)
     const handleOpenModal = () => {
         setOpenModal(!openModal)
     }
+
+    const mdMatches = useMediaQuery("(min-width: 900px)")
 
     return (
         <div className='user-tab'>
@@ -120,18 +137,48 @@ const OrderStatus = () => {
                     open={openModal}
                     onClose={() => setOpenModal(false)}
                 >
-                    <div
+                    <Paper
                         style={{
                             backgroundColor: "#fff",
                             position: "absolute",
                             top: "50%",
                             left: "50%",
                             transform: 'translate(-50%, -50%)',
-                            width: "70%"
+                            width: mdMatches ? "70%" : "95%",
+                            height: "85%",
+                            overflow: "auto",
+                            padding: "20px 30px"
                         }}
                     >
-                        dadsa
-                    </div>
+                        <div>
+                            <div style={{
+                                display: "flex",
+                                flexDirection: "row-reverse",
+                                padding: "10px",
+                            }}>
+                                <CloseOutlined onClick={() => setOpenModal(false)} />
+                            </div>
+                            
+                            <div style={{
+                                display: "flex",
+                                alignItems: "center",
+                                gap: "16px"
+                            }}>
+                                <span style={fontStyle("16px", "#5b5676")}>Subtotal:</span>
+                                <span style={fontStyle("20px", ThemeConfig.palette.primary.main)}>£210</span>
+                            </div>
+                            <div style={{
+                                display: "flex",
+                                alignItems: "center",
+                                gap: "16px"
+                            }}>
+                                <span style={fontStyle("22px", "#5b5676")}>Total: </span>
+                                <span style={fontStyle("26px", ThemeConfig.palette.primary.main)}>£250</span>
+                            </div>
+                        </div>
+                        <Divider sx={{ marginY: "15px" }} />
+                        <ProductCheckout />
+                    </Paper>
                 </Modal>
             </div>
         </div>

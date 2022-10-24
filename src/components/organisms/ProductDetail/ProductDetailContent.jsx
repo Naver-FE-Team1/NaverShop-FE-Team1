04f2574ff713d5ffdfe8dca37203f1cd0db76373
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2022-10-24 16:07:27 +0700

    complete handle api product detail and add color picker function

--- src/components/organisms/ProductDetail/ProductDetailContent.jsx
+++ src/components/organisms/ProductDetail/ProductDetailContent.jsx
@@ -1,10 +1,10 @@
-import {useDispatch} from 'react-redux';
-import { toast } from 'react-toastify';
-import {nanoid} from '@reduxjs/toolkit'
-import {addBasket} from '../../../store/reducers/basketSlice'
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { nanoid } from "@reduxjs/toolkit";
+import { addBasket } from "../../../store/reducers/basketSlice";
 import Grid from "@mui/material/Grid";
 import { Container } from "@mui/system";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMediaQuery } from "@mui/material";
 import React from "react";
 import "../../../scss/ProductDetail/ProductDetailContent.scss";
@@ -14,41 +14,45 @@ import Size from "../../atoms/Size/Size";
 import SubImage from "../../atoms/SubImage/SubImage";
 import SliderSlick from "../../molecules/SliderSlick/SliderSlick";
 import { useNavigate, useParams } from "react-router-dom";
-const ProductDetailContent = ({data}) => {
-  //state này để lưu size S,M,L,...
-  //ban đầu ấn add to basket sẽ lưu vô local
-  const [sizePicker, setSizePicker] = useState(data.sizes[0].size);
-  const [quant, setQuant] = useState(data.quantity);
-  const { id } = useParams(); // id được khai báo ở trang App.jsx
-  //Lấy ra ở đây để dùng trong các trường hợp query 1 sản phẩm theo id
-
+import parse from "html-react-parser";
+const ProductDetailContent = ({
+  data,
+  sizePicker,
+  setSizePicker,
+  setQuant,
+  quant,
+  listImages,
+  setColorPicker,
+  colorPicker,
+}) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const mdMatches = useMediaQuery("(min-width:600px)");
   const lgMatches = useMediaQuery("(min-width:1200px)");
-  
+
   const handleAddtoCart = () => {
-    const productStringify = 
-      { 
-        id: nanoid(), 
-        title: data.title,
-        price: data.price,
-        desc: data.desc,
-        quantity: quant, 
-        sizes: sizePicker,
-        srcImg: data.srcImg[0].src,
-        totalPrice: data.price * quant,
-        productId: data.id,
-      }
-    ;
-    dispatch(addBasket(productStringify))
-    toast.success(`Thêm thành công sản phẩm ${data.title} vào giỏ hàng`)
+    const productStringify = {
+      id: nanoid(),
+      title: data.title,
+      price: data.price,
+      desc: data.desc,
+      quantity: quant,
+      sizes: sizePicker,
+      srcImg: data.srcImg[0].src,
+      totalPrice: data.price * quant,
+      productId: data.id,
+    };
+    dispatch(addBasket(productStringify));
+    toast.success(`Thêm thành công sản phẩm ${data.title} vào giỏ hàng`);
   };
-  //Number
-  var price = 25000000;
-  //subImages
-  const subImages = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
+  const [defaultImage, setDefaultImage] = useState(data.image);
+  const handleChangeDefaultImage = (src) => {
+    setDefaultImage(src);
+  };
+  const handleSetColor = (color) => {
+    setColorPicker(color);
+  };
   return (
     <div className="container">
       <Container style={{ padding: 0 }}>
@@ -56,31 +60,33 @@ const ProductDetailContent = ({data}) => {
           <Grid item xs={12} lg={6}>
             <Grid>
               <img
-                src={data.srcImg[0].src}
+                src={defaultImage ? defaultImage : data.image}
                 className="productDetail-img"
-                alt={data.srcImg[0].alt}
+                alt={defaultImage ? defaultImage : data.image}
               />
             </Grid>
             <Grid item>
-              <SliderSlick showItem={3}>
-                {data.srcImg.map((item) => (
+              <SliderSlick
+                showItem={listImages?.length < 3 ? listImages?.length : 3}
+              >
+                {listImages?.map((item, idx) => (
                   <SubImage
-                    key={item.id}
+                    key={idx}
                     data={item}
+                    onClick={() => {
+                      handleChangeDefaultImage(item);
+                    }}
                   />
                 ))}
               </SliderSlick>
             </Grid>
           </Grid>
-
           <Grid item xs={12} lg={6}>
             <Container maxWidth="sm" style={{ padding: "28px" }}>
               <div className="productDetail__topContent">
-                <h3 className="productDetail__topContent-name">
-                  {data.title}
-                </h3>
+                <h3 className="productDetail__topContent-name">{data.name}</h3>
                 <p className="productDetail__topContent-price">
-                  {data.price.toLocaleString("vi-VN", {
+                  {data?.price?.toLocaleString("vi-VN", {
                     style: "currency",
                     currency: "VND",
                   })}
@@ -91,14 +97,8 @@ const ProductDetailContent = ({data}) => {
                   Product description
                 </h5>
                 <p className="productDetail__description-content">
-                  {data.desc}
+                  {parse(data.description || "")}
                 </p>
-                <ul className="productDetail__description-fabric">
-                  <li>Product description</li>
-                  <li>Product description</li>
-                  <li>Product description</li>
-                </ul>
-                
               </div>
               <div className="productDetail__dimension">
                 <h5 className="productDetail__dimension-title">Dimensions</h5>
@@ -107,18 +107,35 @@ const ProductDetailContent = ({data}) => {
                   spacing={1}
                   sx={{ justifyContent: "space-between" }}
                 >
-                  {data.sizes.map((item) => (
-                    <Grid item key={item.id}>
+                  {data?.sizes?.map((item, idx) => (
+                    <Grid item key={idx}>
                       <Size
-                        picked={sizePicker === item.size}
-                        onClick={() => setSizePicker(item.size)}
-                        size={item.size}
+                        picked={sizePicker === item}
+                        onClick={() => setSizePicker(item)}
+                        size={item}
                       />
                     </Grid>
                   ))}
                 </Grid>
                 <div className="productDetail__dimension-colors"></div>
               </div>
+              <ul className="productDetail__description-colors">
+                {data.color?.map((item, idx) => (
+                  <li
+                    className="productDetail__description-color"
+                    style={{
+                      backgroundColor: item,
+                      border:
+                        colorPicker === item
+                          ? `3px solid ${item}`
+                          : ` 3px #fff solid`,
+                    }}
+                    onClick={() => {
+                      handleSetColor(item);
+                    }}
+                  ></li>
+                ))}
+              </ul>
               <div className="productDetail__quantity">
                 <h5 className="productDetail__quantity-title">Quantity</h5>
                 <Grid container spacing={2}>
--- src/components/pages/ProductDetail/ProductDetail.jsx
+++ src/components/pages/ProductDetail/ProductDetail.jsx
@@ -1,5 +1,15 @@
-import { useEffect } from "react";
-import { productDetail } from "../../organisms/ProductDetail/data";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { db } from "../../../firebase/firebase-config";
+// import { productDetail } from "../../organisms/ProductDetail/data";
 import ProductDetailContent from "../../organisms/ProductDetail/ProductDetailContent";
 import ProductDetailJoin from "../../organisms/ProductDetail/ProductDetailJoin";
 import ProductDetailList from "../../organisms/ProductDetail/ProductDetailList";
@@ -9,12 +19,68 @@ const ProductDetail = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  const [productDetail, setProductDetail] = useState([]);
 
+  console.log(
+    "🚀 ~ file: ProductDetail.jsx ~ line 24 ~ ProductDetail ~ productDetail",
+    productDetail
+  );
+  const { id } = useParams(); // id được khai báo ở trang App.jsx
+  //state này để lưu size S,M,L,...
+  const [sizePicker, setSizePicker] = useState([]);
+  const [colorPicker, setColorPicker] = useState([]);
+  const [quant, setQuant] = useState(0);
+  const [listImages, setListImages] = useState([]);
+
+  console.log(
+    "🚀 ~ file: ProductDetailContent.jsx ~ line 48 ~ listImages",
+    listImages
+  );
+
+  useEffect(() => {
+    (async () => {
+      // if (!id) return;
+      // else {
+      const docRef = doc(db, "products", id);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+        handleGetListProductWithCategories(data.category);
+        setProductDetail(data);
+        setSizePicker(data.sizes[0]);
+        setListImages([data.image, ...data.detailImages]);
+        setColorPicker(data.color[0]);
+      } else {
+        console.log("no data");
+      }
+      // }
+    })();
+  }, [id]);
+
+  const [productCategories, setProductCategories] = useState([]);
+
+  const handleGetListProductWithCategories = async (categories) => {
+    const docRef = collection(db, "products");
+    const que = query(docRef, where("category", "==", categories));
+    const snapshots = await getDocs(que);
+    const docs = snapshots.docs.map((doc) => {
+      const data = doc.data();
+      return data;
+    });
+    setProductCategories(docs);
+  };
   return (
     <>
-      {productDetail.map((product) => (
-        <ProductDetailContent key={product.id} data={product} />
-      ))}
+      <ProductDetailContent
+        setSizePicker={setSizePicker}
+        sizePicker={sizePicker}
+        data={productDetail}
+        setQuant={setQuant}
+        quant={quant}
+        listImages={listImages}
+        setColorPicker={setColorPicker}
+        colorPicker={colorPicker}
+      />
       <ProductDetailList />
       <ProductReview />
       <ProductDetailJoin />

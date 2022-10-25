9858fada075d81578a12204caadfa1eb70cfa108
PhamneyuT <koduapass@gmail.com>
2022-10-25 13:45:33 +0700

    fix

--- src/components/organisms/TabPanelDetail/UserInfo.jsx
+++ src/components/organisms/TabPanelDetail/UserInfo.jsx
@@ -5,7 +5,7 @@
 
 import { Stack, Typography, useMediaQuery } from "@mui/material";
 import { Formik, Form, Field } from "formik";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import OutlinedInput from "../../molecules/TextField/OutlinedInput";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { auth, db } from "../../../firebase/firebase-config";
@@ -15,210 +15,208 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const errorStyle = {
-  marginTop: "5px",
-  marginLeft: "15px",
-  color: "red",
+    marginTop: "5px",
+    marginLeft: "15px",
+    color: "red",
 };
 
 const UserInfo = (props) => {
-  const tabletMatches = useMediaQuery("(min-width: 768px)");
+    const tabletMatches = useMediaQuery("(min-width: 768px)");
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [accountInfo, setAccountInfo] = useState({
-    fullname: "",
-    age: 0,
-    email: "",
-    address: "",
-    phonenumber: "",
-    avatarId: "link",
-  });
-
-  /**
-   * To wait for UserInfo render,
-   * then wait for state changing from auth,
-   * (with onAuthStateChanged, this will return auth the currentUser.uid),
-   * unsubcribe the listener
-   * finally, get the data from the auth.currentUser.uid
-   */
-  useEffect(() => {
-    const unSub = auth.onAuthStateChanged((user) => {
-      unSub();
-      if (!user) {
-        navigate("/");
-      } else {
-        const userRef = doc(db, "users", auth.currentUser.uid);
-        const result = getDoc(userRef)
-          .then((res) => setAccountInfo({ ...res.data() }))
-          .catch((err) => console.log(err));
-      }
+    const [accountInfo, setAccountInfo] = useState({
+        fullname: "",
+        age: 0,
+        email: "",
+        address: "",
+        phonenumber: "",
+        avatarId: "link",
     });
-  }, []);
 
-  return (
-    <div className="user-tab">
-      <Typography variant="h3">My Account</Typography>
-      <Formik
-        enableReinitialize
-        initialValues={accountInfo}
-        validate={(values) => {
-          const errors = {};
+    const formRef = useRef()
 
-          //regex for fullname
-          if (
-            values.fullname &&
-            !/^[a-zA-Z0-9ỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ' ]+$/.test(
-              values.fullname
-            )
-          ) {
-            errors.fullname = "Invalid name";
-          }
+    const handleSubmit = async () => {
+        const data = formRef.current?.values
 
-          if (values.age && !/^\d+$/.test(values.age)) {
-            errors.age = "Age can only be contained of numbers";
-          }
+        const userRef = doc(db, "users", auth.currentUser?.uid)
+        await updateDoc(userRef, {
+            ...data
+        })
 
-          //regex for email
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = "Invalid email address";
-          }
+        toast.success("Cập nhật thông tin thành công!")
+    }
 
-          //regex for phonenumber
-          if (values.phonenumber && !/^\d+$/.test(values.phonenumber)) {
-            errors.phonenumber =
-              "Phone number can only be contained of numbers";
+    /**
+     * To wait for UserInfo render,
+     * then wait for state changing from auth,
+     * (with onAuthStateChanged, this will return auth the currentUser.uid),
+     * unsubcribe the listener
+     * finally, get the data from the auth.currentUser.uid
+     */
+    useEffect(() => {
+        const unSub = auth.onAuthStateChanged((user) => {
+            unSub();
+            if (!user) {
+                navigate("/");
+            } else {
+                const userRef = doc(db, "users", auth.currentUser.uid);
+                const result = getDoc(userRef)
+                    .then((res) => setAccountInfo({ ...res.data() }))
+                    .catch((err) => console.log(err));
+            }
+        });
+    }, []);
 
-            // if (values.phonenumber.length < 10 || values.phonenumber.length > 11) {
-            //     errors.phonenumber = "Phone number can only be consisted of 10 or 11 numbers"
-            // }
-          }
+    return (
+        <div className="user-tab">
+            <Typography variant="h3">My Account</Typography>
+            <Formik
+                innerRef={formRef}
+                enableReinitialize
+                initialValues={accountInfo}
+                validate={(values) => {
+                    const errors = {};
 
-          return errors;
-        }}
-        onSubmit={async (values) => {
-          /*
-           * If onSubmit is async,
-           * then Formik will automatically set isSubmitting to false
-           * on your behalf once it has resolved
-           */
-          const userRef = doc(db, "users", auth.currentUser?.uid);
-          await updateDoc(userRef, {
-            ...values,
-          });
+                    //regex for fullname
+                    if (
+                        values.fullname &&
+                        !/^[a-zA-Z0-9ỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ' ]+$/.test(
+                            values.fullname
+                        )
+                    ) {
+                        errors.fullname = "Invalid name";
+                    }
 
-          toast.success("Cập nhật thông tin thành công!");
-        }}
-      >
-        {(props) => (
-          <Form
-            style={{
-              width: "100%",
-              marginTop: "15px",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: "25px",
-              }}
+                    if (values.age && !/^\d+$/.test(values.age)) {
+                        errors.age = "Age can only be contained of numbers";
+                    }
+
+                    //regex for email
+                    if (!values.email) {
+                        errors.email = "Required";
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                        errors.email = "Invalid email address";
+                    }
+
+                    //regex for phonenumber
+                    if (values.phonenumber && !/^\d+$/.test(values.phonenumber)) {
+                        errors.phonenumber =
+                            "Phone number can only be contained of numbers";
+
+                    }
+
+                    return errors;
+                }}
             >
-              <AvatarButton data={props.values.avatarId} />
-              <Stack sx={{ width: "100%" }} spacing={3}>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: tabletMatches ? "row" : "column",
-                    gap: tabletMatches ? "10px" : "25px",
-                  }}
-                >
-                  <Field name="fullname">
-                    {({ field, form, meta }) => (
-                      <div style={{ width: "100%" }}>
-                        <OutlinedInput
-                          label="Full name"
-                          type="text"
-                          style={{ width: "100%" }}
-                          {...field}
-                        />
-                        {meta.touched && meta.error && (
-                          <div style={errorStyle}>{meta.error}</div>
-                        )}
-                      </div>
-                    )}
-                  </Field>
-                  <Field name="age">
-                    {({ field, form, meta }) => (
-                      <div>
-                        <OutlinedInput
-                          label="Age"
-                          type="text"
-                          style={{ width: "100%" }}
-                          {...field}
-                        />
-                        {meta.touched && meta.error && (
-                          <div style={errorStyle}>{meta.error}</div>
-                        )}
-                      </div>
-                    )}
-                  </Field>
-                </div>
-                <Field name="email">
-                  {({ field, form, meta }) => (
-                    <>
-                      <OutlinedInput
-                        label="Email"
-                        type="email"
-                        disabled={true}
-                        {...field}
-                      />
-                      {meta.touched && meta.error && (
-                        <div style={errorStyle}>{meta.error}</div>
-                      )}
-                    </>
-                  )}
-                </Field>
-                <Field name="address">
-                  {({ field, form, meta }) => (
-                    <>
-                      <OutlinedInput label="Address" type="text" {...field} />
-                      {meta.touched && meta.error && (
-                        <div style={{ ...errorStyle }}>{meta.error}</div>
-                      )}
-                    </>
-                  )}
-                </Field>
-                <Field name="phonenumber">
-                  {({ field, form, meta }) => (
-                    <>
-                      <OutlinedInput
-                        label="Phone Number"
-                        type="text"
-                        {...field}
-                      />
-                      {meta.touched && meta.error && (
-                        <div style={{ ...errorStyle }}>{meta.error}</div>
-                      )}
-                    </>
-                  )}
-                </Field>
-              </Stack>
-            </div>
+                {(props) => (
+                    <Form
+                        style={{
+                            width: "100%",
+                            marginTop: "15px",
+                        }}
+                    >
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                gap: "25px",
+                            }}
+                        >
+                            <AvatarButton data={props.values.avatarId} />
+                            <Stack sx={{ width: "100%" }} spacing={3}>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: tabletMatches ? "row" : "column",
+                                        gap: tabletMatches ? "10px" : "25px",
+                                    }}
+                                >
+                                    <Field name="fullname">
+                                        {({ field, form, meta }) => (
+                                            <div style={{ width: "100%" }}>
+                                                <OutlinedInput
+                                                    label="Full name"
+                                                    type="text"
+                                                    style={{ width: "100%" }}
+                                                    {...field}
+                                                />
+                                                {meta.touched && meta.error && (
+                                                    <div style={errorStyle}>{meta.error}</div>
+                                                )}
+                                            </div>
+                                        )}
+                                    </Field>
+                                    <Field name="age">
+                                        {({ field, form, meta }) => (
+                                            <div>
+                                                <OutlinedInput
+                                                    label="Age"
+                                                    type="text"
+                                                    style={{ width: "100%" }}
+                                                    {...field}
+                                                />
+                                                {meta.touched && meta.error && (
+                                                    <div style={errorStyle}>{meta.error}</div>
+                                                )}
+                                            </div>
+                                        )}
+                                    </Field>
+                                </div>
+                                <Field name="email">
+                                    {({ field, form, meta }) => (
+                                        <>
+                                            <OutlinedInput
+                                                label="Email"
+                                                type="email"
+                                                disabled={true}
+                                                {...field}
+                                            />
+                                            {meta.touched && meta.error && (
+                                                <div style={errorStyle}>{meta.error}</div>
+                                            )}
+                                        </>
+                                    )}
+                                </Field>
+                                <Field name="address">
+                                    {({ field, form, meta }) => (
+                                        <>
+                                            <OutlinedInput label="Address" type="text" {...field} />
+                                            {meta.touched && meta.error && (
+                                                <div style={{ ...errorStyle }}>{meta.error}</div>
+                                            )}
+                                        </>
+                                    )}
+                                </Field>
+                                <Field name="phonenumber">
+                                    {({ field, form, meta }) => (
+                                        <>
+                                            <OutlinedInput
+                                                label="Phone Number"
+                                                type="text"
+                                                {...field}
+                                            />
+                                            {meta.touched && meta.error && (
+                                                <div style={{ ...errorStyle }}>{meta.error}</div>
+                                            )}
+                                        </>
+                                    )}
+                                </Field>
+                            </Stack>
+                        </div>
 
-            <div style={{ display: "flex", justifyContent: "flex-end" }}>
-              <MuiCustomButton onClick={props.submitForm} type="submit">
-                Save
-              </MuiCustomButton>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+                        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                            <MuiCustomButton onClick={handleSubmit} type="submit">
+                                Save
+                            </MuiCustomButton>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
 };
 
 export default UserInfo;
